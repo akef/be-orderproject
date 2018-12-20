c8a41d2806e4293b83948ac6aad705efe74abd51
mustafa <mustafa.3akef@gmail.com>
2018-12-20 12:54:09 -0800

    intial commit

---
+++ be-orderproject/src/main/java/com/test/order/BeOrderprojectApplication.java
@@ -0,0 +1,14 @@
+package com.test.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeOrderprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeOrderprojectApplication.class, args);
+	}
+
+}
+
